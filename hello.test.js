826884a0baf180e4fec17a2148f6212b7cb0ee05
N1kitaGordeev <122966658+N1kitaGordeev@users.noreply.github.com>
2024-04-16 17:43:00 +0300

    Update hello.test.js

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Qatar!");
+    expect(hello.hello()).toEqual("Hello World from the Qatar!");
   });
 });
